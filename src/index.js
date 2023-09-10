bdc54cb356c6ac1851458d3c3f83baee16cae881
cmfernandes <cristianafmfernandes@gmail.com>
2023-09-10 20:38:32 +0100

    add menubtn event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import displayMenu from "./menu.js"
 import {webpageInit} from "./webpage.js"
 
+
+//console.log(createNav)
 webpageInit()
+
+const menuBtn = document.querySelector(".menu")
+
+menuBtn.addEventListener('click', displayMenu)
+
+
+
