941d1e73e6c88880e3f8865616f6d48c5c0b7b76
cmfernandes <cristianafmfernandes@gmail.com>
2023-09-12 00:04:48 +0100

    add two divs

--- src/home.js
+++ src/home.js
@@ -3,28 +3,37 @@ function createHome(){
     const home = document.createElement("div")
     home.classList.add("home")
 
-    const mainImg = document.createElement("img")
-    mainImg.src = "/src/images/restaurant-img.jpg"
-    mainImg.alt = "Restaurant"
+    const homeImgCont = document.createElement("div");
+    homeImgCont.classList.add("home-img-cont")
 
-    const restName = document.createElement("h1")
-    restName.textContent = "Fancy Burger"
+    const mainImg = document.createElement("img");
+    mainImg.src = "/src/images/restaurant-img.jpg";
+    mainImg.alt = "Restaurant";
 
-    const headline = document.createElement("h2")
-    headline.classList.add("headline")
-    headline.textContent = "Where real burgers are made!"
+    homeImgCont.appendChild(mainImg)
 
+    const headlineCont  = document.createElement("div");
+    headlineCont.classList.add("headline-cont")
 
-    home.appendChild(mainImg)
-    home.appendChild(restName)
-    home.appendChild(headline)
+    const restName = document.createElement("h1");
+    restName.textContent = "Fancy Burger";
+
+    const headline = document.createElement("h2");
+    headline.classList.add("headline");
+    headline.textContent = "Where real burgers are made!";
+
+    headlineCont.appendChild(restName);
+    headlineCont.appendChild(headline);
+
+    home.appendChild(homeImgCont);
+    home.appendChild(headlineCont);
 
     return home
 }
 
 export default function displayHome(){
 
-    const mainCont = document.querySelector(".main-content")
+    const mainCont = document.querySelector(".main-content");
     
     mainCont.textContent = "";
     mainCont.appendChild(createHome());
