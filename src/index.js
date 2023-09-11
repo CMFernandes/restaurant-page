55c9427f08cfcfc2950bedcec5568ea48beff3dd
cmfernandes <cristianafmfernandes@gmail.com>
2023-09-11 14:38:24 +0100

    add btns event listeners

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import displayHome from "./home.js"
+import displayContact from "./contact.js"
 import displayMenu from "./menu.js"
 import {webpageInit} from "./webpage.js"
 
 
-//console.log(createNav)
 webpageInit()
 
-const menuBtn = document.querySelector(".menu")
+const menuBtn = document.querySelector(".menuBtn");
+menuBtn.addEventListener('click', displayMenu);
 
-menuBtn.addEventListener('click', displayMenu)
+const contactBtn = document.querySelector(".contactBtn");
+contactBtn.addEventListener('click', displayContact);
 
+const homeBtn = document.querySelector(".homeBtn");
+homeBtn.addEventListener('click', displayHome);
 
 
